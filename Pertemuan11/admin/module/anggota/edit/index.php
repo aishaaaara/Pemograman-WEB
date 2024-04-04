9002dddcf81d3290943ac987674c3c05361a065e
Aisha Rahmadia <aisharahmadia8@gmail.com>
2024-04-05 00:13:58 +0700

    Pertemuan11

--- Pertemuan11/admin/module/anggota/edit/index.php
+++ Pertemuan11/admin/module/anggota/edit/index.php
@@ -1,37 +1,57 @@
+<?php
+// Memulai sesi
+session_start();
+
+// Memeriksa apakah pengguna sudah login
+if (!empty($_SESSION['username'])) {
+    // Memasukkan file-file yang diperlukan
+    require 'admin/template/menu.php';
+
+    // Mengambil ID anggota dari parameter GET
+    $id = $_GET['id'];
+
+    // Mengambil informasi anggota dari database
+    $query = "SELECT * FROM anggota a, jabatan j, user u WHERE a.jabatan_id = j.id AND a.user_id = u.id AND a.user_id = '$id'";
+    $result = mysqli_query($koneksi, $query);
+    $row = mysqli_fetch_assoc($result);
+?>
+
+<!-- Mulai bagian utama HTML -->
 <div class="container-fluid">
     <div class="row">
-        <?php
-        require 'admin/template/menu.php';
-        $id = $_GET['id'];
-        $query = "SELECT * FROM anggota a, jabatan j, user u WHERE a.jabatan_id = j.id AND a.user_id = u.id AND a.user_id = '$id'";
-        $result = mysqli_query($koneksi, $query);
-        $row = mysqli_fetch_assoc($result);
-        ?>
+        <!-- Menampilkan menu -->
+        <?php require 'admin/template/menu.php'; ?>
 
+        <!-- Bagian utama untuk mengedit anggota -->
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Anggota</h1>
             </div>
+            <!-- Form untuk mengubah informasi anggota -->
             <form action="fungsi/edit.php?anggota=edit" method="POST">
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="card">
                             <div class="card-header">Form Edit Anggota</div>
                             <div class="card-body">
+                                <!-- Input untuk nama anggota -->
                                 <input type="hidden" value="<?php echo $row['user_id']; ?>" name="id">
                                 <div class="mb-3">
                                     <label for="nama" class="form-label">Nama</label>
                                     <input type="text" class="form-control" name="nama" value="<?= $row['nama']; ?>">
                                 </div>
+                                <!-- Dropdown untuk memilih jabatan -->
                                 <div class="mb-3">
                                     <label for="jabatan" class="form-label">Jabatan:</label>
                                     <select class="form-select" name="jabatan" aria-label="Default select example">
                                         <option selected>Pilih Jabatan</option>
                                         <?php
+                                        // Mengambil daftar jabatan dari database
                                         $query2 = "SELECT * FROM jabatan order by jabatan asc";
                                         $result2 = mysqli_query($koneksi, $query2);
                                         while ($row2 = mysqli_fetch_assoc($result2)) {
                                             ?>
+                                            <!-- Opsi untuk setiap jabatan -->
                                             <option value="<?= $row2['id']; ?>" <?= ($row['jabatan_id'] == $row2['id']) ? 'selected' : '' ?>>
                                                 <?= $row2['jabatan']; ?>
                                             </option>
@@ -39,9 +59,11 @@
                                         ?>
                                     </select>
                                 </div>
+                                <!-- Pilihan untuk jenis kelamin -->
                                 <div class="mb-3">
                                     <label class="form-label">Jenis Kelamin:</label>
                                     <br>
+                                    <!-- Checkbox untuk jenis kelamin -->
                                     <div class="form-check form-check-inline">
                                         <input class="form-check-input" type="radio" name="jenis_kelamin" value="L" <?= ($row['jenis_kelamin'] === "L") ? 'checked' : '' ?>>
                                         <label class="form-check-label" for="inlineRadio1">Laki-Laki</label>
@@ -51,10 +73,12 @@
                                         <label class="form-check-label" for="inlineRadio2">Perempuan</label>
                                     </div>
                                 </div>
+                                <!-- Input untuk alamat -->
                                 <div class="mb-3">
                                     <label for="alamat" class="form-label">Alamat</label>
                                     <textarea class="form-control" name="alamat"><?= $row['alamat']; ?></textarea>
                                 </div>
+                                <!-- Input untuk nomor telepon -->
                                 <div class="mb-3">
                                     <label for="no_telp" class="form-label">No Telepon</label>
                                     <input type="number" class="form-control" name="no_telp" value="<?= $row['no_telp']; ?>">
@@ -68,10 +92,12 @@
                                 Form Edit Login Anggota
                             </div>
                             <div class="card-body">
+                                <!-- Input untuk username -->
                                 <div class="mb-3">
                                     <label for="username" class="form-label">Username</label>
                                     <input type="text" class="form-control" name="username" value="<?= $row['username']; ?>">
                                 </div>
+                                <!-- Input untuk password -->
                                 <div class="mb-3">
                                     <label for="password" class="form-label">Password</label>
                                     <input type="password" class="form-control" name="password">
@@ -86,8 +112,10 @@
                     <div class="col-sm-12">
                         <div class="card">
                             <div class="card-body" style="text-align: center;">
+                                <!-- Tombol untuk menyimpan perubahan -->
                                 <button type="submit" class="btn btn-primary"><i class="fa fa-floppy-o"
                                         aria-hidden="true"></i> Ubah</button>
+                                <!-- Tombol untuk membatalkan -->
                                 <a href="index.php?page=anggota" class="btn btn-secondary"><i class="fa fa-times"
                                         aria-hidden="true"></i> Batal</a>
                             </div>
@@ -98,3 +126,4 @@
         </main>
     </div>
 </div>
+<!-- Akhir bagian utama HTML -->
